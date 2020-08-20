7f24036b2cc0a3781920da78c1c214008a624c65
STORYM <ssashkaa01@gmail.com>
2020-08-20 11:50:13 +0300

    change view homeController

--- Web.Shop/Controllers/HomeController.cs
+++ Web.Shop/Controllers/HomeController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Web.Shop.Entities;
 using Web.Shop.Models;
 
 namespace Web.Shop.Controllers
@@ -12,17 +14,26 @@ namespace Web.Shop.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<DbUser> _userManager; 
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<DbUser> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
+           
         }
 
         public IActionResult Index()
         {
+            var list = _userManager.Users.Select(u => new UserVM
+            {
+                Id = u.Id,
+                Email = u.Email
+            }).ToList();
+            
            
             _logger.LogWarning("Привіт. Це home -> index :)");
-            return View();
+            return View(list);
         }
 
         public IActionResult Privacy()
--- Web.Shop/Models/UserVM.cs
+++ Web.Shop/Models/UserVM.cs
@@ -7,7 +7,7 @@ namespace Web.Shop.Models
 {
     public class UserVM
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
         public string Email { get; set; }
 
     }
