affc74e155095bcadbc51b3625ef2ca0372a7ecb
Volodymyr Novak <novakvova@gmail.com>
2020-09-14 11:49:00 +0300

    Add new app grafics

---
+++ Web.Shop/Controllers/StatisticsController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Web.Shop.Models;
+
+namespace Web.Shop.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        public IActionResult Index()
+        {
+            StatisticsVM model = new StatisticsVM();
+            model.Title = "Наша Бригада";
+            model.ListData = new List<DataChartVM>
+            {
+                new DataChartVM
+                {
+                    Id = 1,
+                    Value= "100",
+                    Label = "20 серпня 2019р."
+                },
+                new DataChartVM
+                {
+                    Id = 2,
+                    Value= "300",
+                    Label = "21 серпня 2019р."
+                },
+                new DataChartVM
+                {
+                    Id = 3,
+                    Value= "200",
+                    Label = "22 серпня 2019р."
+                },
+                new DataChartVM
+                {
+                    Id = 3,
+                    Value= "250",
+                    Label = "23 серпня 2019р."
+                },
+                new DataChartVM
+                {
+                    Id = 4,
+                    Value= "50",
+                    Label = "24 серпня 2019р."
+                },
+                new DataChartVM
+                {
+                    Id = 4,
+                    Value= "-20",
+                    Label = "25 серпня 2019р."
+                },
+                new DataChartVM
+                {
+                    Id = 4,
+                    Value= "400",
+                    Label = "26 серпня 2019р."
+                },
+            };
+            return View(model);
+        }
+    }
+}
---
+++ Web.Shop/Models/StatisticsVM.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Shop.Models
+{
+    public class StatisticsVM
+    {
+        public string Title { get; set; }
+        public List<DataChartVM> ListData { get; set; }
+    }
+    public class DataChartVM
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+        public string Label { get; set; }
+    }
+}
